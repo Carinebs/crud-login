4b756d2f3e621193acfca29a1e2fc6f4c9f5c038
Carine <86265525+Carinebs@users.noreply.github.com>
2023-04-28 17:24:22 -0300

    adicionando o .env para informações do db

--- back-end/src/database/data-source.ts
+++ back-end/src/database/data-source.ts
@@ -2,15 +2,16 @@ import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { UserTable1682635703325 } from './migrations/1682635703325-UserTable';
 import User from "../app/entities/User";
-
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
+    host: process.env.DB_HOST,
     port: 5432,
-    username: "postgres",
-    password: "310520",
-    database: "postgres",
+    username:process.env.DB_NAME,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE,
     synchronize: true,
     logging: false,
     entities: [User],
