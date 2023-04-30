4128cdb660a7226e6641d656a03c8d39ebbd8fa8
Carine <86265525+Carinebs@users.noreply.github.com>
2023-04-30 16:35:32 -0300

    Deixando rota de user privada

--- meu-app-do-react/src/App.js
+++ meu-app-do-react/src/App.js
@@ -1,27 +1,21 @@
-import { createBrowserRouter, RouterProvider} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Login from './views/Login';
 import Register from './views/Register';
 import User from './views/User';
+import PrivateRoutes from "./utils/PrivateRoutes";
 
-const router  = createBrowserRouter([
-  {
-   path: "/",
-   element: <Login />
-  },
-  {
-    path: "/register",
-    element: <Register />
-   },
-   {
-    path: "/user",
-    element: <User />
-   }
-
-]);
 
 function App() {
   return (
-   <RouterProvider router={router} />
+    <Router>
+    <Routes>
+      <Route element={<PrivateRoutes/>}>
+          <Route path='/user' element={<User/>} />
+      </Route>
+      <Route path='/' element={<Login/>}/>
+      <Route path='/register' element={<Register/>}/>
+    </Routes>
+</Router>
   );
 }
 
---
+++ meu-app-do-react/src/utils/PrivateRoutes.jsx
@@ -0,0 +1,17 @@
+
+import { useState } from 'react'
+import { Navigate, Outlet } from 'react-router-dom'
+
+
+
+const PrivateRoutes = () => {
+    let auth = {'token': false}
+
+
+    return (
+        auth.token ? <Outlet/> : <Navigate to='/login'/>
+    )
+}
+
+
+export default PrivateRoutes;
